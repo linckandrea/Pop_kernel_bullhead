68dc3fac2760d4cf03521a82090117299d66a053
Andrea <linckandrea01@gmail.com>
2020-09-07 23:09:49 +0200

    make my changes

--- build.sh
+++ build.sh
@@ -1,44 +1,30 @@
-#!/bin/sh
+echo
+echo "Setup"
+echo
+export LOCALVERSION="-Pop-Kernel-${branch_name}/${last_commit}"
+mkdir -p out
+export ARCH=arm64
+export SUBARCH=arm64
+make O=out clean
+make O=out mrproper
 
-# Variables
-branch_name=$(git rev-parse --abbrev-ref HEAD)
-last_commit=$(git rev-parse --verify --short=8 HEAD)
-export LOCALVERSION="-Shadow-Kernel-${branch_name}/${last_commit}"
-cpus=$(nproc --all)
-Red='\033[0;31m'
-Blue='\033[0;34m'
-Color_Off='\033[0m'
-defconfig="shadow_defconfig"
-spaces="  "
+echo
+echo "Issue Build Commands"
+echo
+export CROSS_COMPILE=/home/andrea/android/gcc10/arm64-gcc/bin/aarch64-elf-
 
-die()
-{
-	echo "$spaces | $Red${1} |"
-	exit 1
-}
+echo
+echo "Set DEFCONFIG"
+echo 
+make O=out shadow_defconfig
 
-# fire it!
-echo "\n$spaces| ${Blue}Kernel Compilation Started!${Color_Off} |"
+echo
+echo "Build The Good Stuff"
+echo 
+make O=out -j$(nproc --all)
 
-# make defconfig
-if ! make -s "$defconfig"; then
-	die "Defconfig Generation Failed!"
-fi
-
-# make kernel
-if ! make CROSS_COMPILE=aarch64-linux-gnu- -j "${cpus}"; then
-	die "Kernel Compilation failed!"
-fi
-
-# set version
-date=$(date +%m%d%H%M)
-version="Shadow-${date}-${last_commit}"
-
-# create ramdisk
-cp arch/arm64/boot/Image.gz-dtb "$HOME"/bullhead/AnyKernel
-cd "$HOME"/bullhead/AnyKernel || die "cd to anykernel dir failed"
-echo "$spaces| ${Blue}Zipping!${Color_Off} |"
-zip "$version".zip -r ./* -x \*.zip
-echo "$spaces| ${Blue}Done! -> ""$version"".zip${Color_Off} |"
-
-notify-send "Kernel" "Compilation is done!"
+rm ./AnyKernel3/Image.gz-dtb
+cp ./out/arch/arm64/boot/Image.gz-dtb ./AnyKernel3
+cd AnyKernel3
+rm ./Pop_kernel-bullhead-O-rx-x.zip
+zip -r9 Pop_kernel-bullhead-O-rx-x.zip * -x .git README.md *placeholder
