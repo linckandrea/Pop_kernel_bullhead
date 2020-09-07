9dd4142e6656e3496fdde6d98544688e1274ce17
Andrea <linckandrea01@gmail.com>
2020-09-07 23:09:49 +0200

    make my changes

--- build.sh
+++ build.sh
@@ -1,47 +1,25 @@
-#!/bin/bash
-date=$(date +%m%d%H%M)
-branch_name=$(git rev-parse --abbrev-ref HEAD)
-last_commit=$(git rev-parse --verify --short=8 HEAD)
-version="Shadow-${date}-${last_commit}"
-cpus=$(nproc --all)
-export LOCALVERSION="-Shadow-Kernel-${branch_name}/${last_commit}"
-RED='\033[0;31m'
-Green='\033[0;32m'
-Yellow='\033[0;33m'
-Blue='\033[0;34m'
-Color_Off='\033[0m'
+echo
+echo "Setup"
+echo
+export LOCALVERSION="-Pop-Kernel-${branch_name}/${last_commit}"
+mkdir -p out
+export ARCH=arm64
+export SUBARCH=arm64
+make O=out clean
+make O=out mrproper
 
-function die()
-{
-	echo -e ${RED}
-	echo "${1}" | boxes -d stone
-	exit
-}
+echo
+echo "Issue Build Commands"
+echo
+export CROSS_COMPILE=/home/andrea/gcc10/arm64-gcc//bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=/home/andrea/gcc10/arm32-gcc//bin/arm-eabi-
 
-cd $HOME/bullhead
-echo -e ${Blue}
-echo "Kernel Compilation Started!" |boxes -d stone
-echo -e ${Color_Off}
-# making defconfig
-make shadow_defconfig
-if [ ! $? -eq 0 ]; then
-	die "Defconfig Generation Failed!"
-fi
+echo
+echo "Set DEFCONFIG"
+echo 
+make O=out shadow_defconfig
 
-# making kernel
-make CROSS_COMPILE=aarch64-linux-gnu- -j ${cpus}
-if [ ! $? -eq 0 ]; then
-	die "Kernel Compilation failed!"
-fi
-
-# AnyKernel
-cp arch/arm64/boot/Image.gz-dtb $HOME/AnyKernel
-cd $HOME/AnyKernel
-echo -e ${Blue}
-echo "Zipping!" |boxes -d stone
-echo -e ${Color_Off}
-zip ${version}.zip -r * -x \*.zip
-echo -e ${Blue}
-echo "Done! -> ${version}.zip" |boxes -d stone
-echo -e ${Color_Off}
-exit
+echo
+echo "Build The Good Stuff"
+echo 
+make O=out -j$(nproc --all)
