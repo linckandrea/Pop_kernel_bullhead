305318835a13484ed4f550ae9516637de14695e9
Andrea <linckandrea01@gmail.com>
2020-09-09 14:26:11 +0200

    fs/sync: fix derp

--- fs/sync.c
+++ fs/sync.c
@@ -205,14 +205,14 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 		return 0;
 	else {
 #endif
-        if (!file->f_op || !file->f_op->fsync)
-            return -EINVAL;
-            if (!datasync && (inode->i_state & I_DIRTY_TIME)) {
-                spin_lock(&inode->i_lock);
-                inode->i_state &= ~I_DIRTY_TIME;
-                spin_unlock(&inode->i_lock);
-                mark_inode_dirty_sync(inode);
-            }
+		if (!file->f_op || !file->f_op->fsync)
+			return -EINVAL;
+		if (!datasync && (inode->i_state & I_DIRTY_TIME)) {
+			spin_lock(&inode->i_lock);
+			inode->i_state &= ~I_DIRTY_TIME;
+			spin_unlock(&inode->i_lock);
+			mark_inode_dirty_sync(inode);
+		}
         return file->f_op->fsync(file, start, end, datasync);
 #ifdef CONFIG_DYNAMIC_FSYNC
 	}
